9d6a20048a77e3df8d77e82433dcfe996cce4c23
guwahatimyvillage-web <guwahatimyvillage@gmail.com>
2026-07-07 00:24:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'providers/auth_provider.dart';
 import 'screens/auth_screen.dart';
 import 'theme/app_theme.dart';
 import 'theme/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const VedoApp());
 }
 
